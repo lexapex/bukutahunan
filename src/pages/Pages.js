4e5cb151eacce5ff2425e7a75e406de6552cdeab
angkyakdifp <akdiangky@gmail.com>
2023-09-26 22:12:34 +0700

    add repo

---
+++ src/pages/Pages.js
@@ -0,0 +1,438 @@
+import Aika from "../assets/Aika.JPG"
+import Chentya from "../assets/Chentya.JPG"
+import Elves from "../assets/Elves.JPG"
+import Valen from "../assets/Valen.JPG"
+import Luwis from "../assets/Luwis.JPG"
+import Anne from "../assets/anne.JPG"
+import Auryna from "../assets/Auryna.JPG"
+import Michael from "../assets/Michael.JPG"
+import Meriana from "../assets/Meriana.JPG"
+import Gerva from "../assets/Gervasius.JPG"
+import Shinta from "../assets/Shinta.JPG"
+import Errin from "../assets/Erin.JPG"
+import Grace from "../assets/Grace.JPG"
+import Filbert from "../assets/Filbert.JPG"
+import Goldwyn from "../assets/Goldwyn.JPG"
+import Melvern from "../assets/Melvern.JPG"
+import Angky from "../assets/Angky.JPG"
+import FA from "../assets/FA.JPG"
+import Nicholas from "../assets/Nicholas.JPG"
+import Richard from "../assets/Richard.JPG"
+import Milie from "../assets/Milie.JPG"
+import FG from "../assets/FG.JPG"
+import aby from "../assets/Aby.JPG"
+import Calvin from "../assets/Calvin.JPG"
+import Keefe from "../assets/Keefe.JPG"
+import Willyanto from "../assets/Willyanto.JPG"
+import Steven from "../assets/Steven.JPG"
+import Irene from "../assets/Irene.JPG"
+import Enriq from "../assets/Enriq.JPG"
+import FE from "../assets/FE.JPG"
+import Erick from "../assets/Erick.JPG"
+import David from "../assets/David.JPG"
+import Widya from "../assets/Widya.JPG"
+import Vallencia from "../assets/Valencia.JPG"
+import FGC from "../assets/FGC.JPG"
+import Patrick from "../assets/Patrick.JPG"
+import Vivin from "../assets/Vivin.JPG"
+import Jillian from "../assets/Jillian.JPG"
+import FB from "../assets/FB.JPG"
+import Katty from "../assets/Katty.JPG"
+import Miko from "../assets/Miko.JPG"
+import Vinson from "../assets/Vinson.JPG"
+import Winston from "../assets/Winston.JPG"
+import Queen from "../assets/Queen.JPG"
+import Witama from "../assets/witama.png"
+export default function Pages() {
+    const Studnet = [1,2,3,4];
+    const Student = [
+        {
+            Nama: "Aika Stevania",
+            TTL: "Pekanbaru, 19 Oktober 2004",
+            Sosmed: "@aikastev",
+            Quotes: "ignorance is just a lesson to be learned",
+            photo: Aika,
+            class: 'IPS'
+        },
+        {
+            Nama: "Chentya Sulianto",
+            TTL: "Pekanbaru, 08 Februari 2004",
+            Sosmed: "@ch.nt.y",
+            Quotes: "1:07 (NA NA NA - Alsa)",
+            photo: Chentya,
+            class: 'IPS'
+        },
+        {
+            Nama: "Valen Claudia Chuardi",
+            TTL: "Medan, 26 February 2005",
+            Sosmed: "@valenclaudia.c",
+            Quotes: "Hello. I am Baymax, your personal healthcare companion.",
+            photo: Valen,
+            class: 'IPS'
+        },
+        {
+            Nama: "Elves Oktavianus",
+            TTL: "Pekanbaru, 26 Oktober 2004",
+            Sosmed: "@elves_okt",
+            Quotes: "Dulu jadi ikan sekarang jadi nelayan",
+            photo: Elves,
+            class: 'IPS'
+        },
+        {
+            Nama: "Auryna Angelica Clara",
+            TTL: "Pekanbaru, 6 Agustus 2004",
+            Sosmed: "@auryna_clara",
+            Quotes: "Have Courage and Be Kind",
+            photo: Auryna,
+            class: 'IPS'
+        },
+        {
+            Nama: "Luwis Hengdriano",
+            TTL: "Pekanbaru, 12 Februari 2004",
+            Sosmed: "@luwis_hengdriano",
+            Quotes: "gmn kabar? ud pada sukses? BLOM YA? HAHAHAHA gpp, gagal itu biasa broh",
+            photo: Luwis,
+            class: 'IPS'
+        },
+        {
+            Nama: "Anne Engrasia Wibowo",
+            TTL: "Pekanbaru, 12 Juni 2004",
+            Sosmed: "-",
+            Quotes: "Nothing's perfect",
+            photo: Anne,
+            class: 'IPS'
+        },
+        {
+            Nama: "Michael",
+            TTL: "Pekanbaru, 14 Juli 2004",
+            Sosmed: "@Archlyzz",
+            Quotes: "Tidak mungkin kamu bisa mengendalkan angin, tetapi kamu bisa mengendalikan perahumu. - Anthony Robbins",
+            photo: Michael,
+            class: 'IPA'
+        },
+        {
+            Nama: "Meriana",
+            TTL: "Pekanbaru, 22 Desember 2004",
+            Sosmed: "@itsme__merry",
+            Quotes: "Be grateful and enjoy the little things in life",
+            photo: Meriana,
+            class: 'IPA'
+        },
+        {
+            Nama: "Gervasius Sebastian Halim",
+            TTL: "Pekanbaru, 30 Juni 2004",
+            Sosmed: "@gerva.s.h",
+            Quotes: "Follow your dreams",
+            photo: Gerva,
+            class: 'IPA'
+        },
+        {
+            Nama: "Shinta",
+            TTL: "Pekanbaru, 10 Mei 2004",
+            Sosmed: "@shxntaa",
+            Quotes: "was released from her 15 years sentence ( ‾́ ◡ ‾́ )",
+            photo: Shinta,
+            class: 'IPS'
+        },
+        {
+            Nama: "Errin Rossania",
+            TTL: "Dumai, 5 Februari 2004",
+            Sosmed: "@errinrossa",
+            Quotes: "Dear Future Me , Love the life you live And Live the life you love",
+            photo: Errin,
+            class: 'IPS'
+        },
+        {
+            Nama: "Grace Theresa",
+            TTL: "Pekanbaru, 22 Agustus 2004",
+            Sosmed: "@grace.tzz",
+            Quotes: "Friendship No. 1",
+            photo: Grace,
+            class: 'IPA'
+        },
+        {
+            Nama: "Filbert Hachman",
+            TTL: "Pekanbaru, 26 Oktober 2004",
+            Sosmed: "@filberth_",
+            Quotes: "jika kamu gagal menjadi orang sukses,berarti kamu sukses menjadi orang gagal",
+            photo: Filbert,
+            class: 'IPA'
+        },
+        {
+            Nama: "Goldwyn Raymond",
+            TTL: "Pekanbaru, 3 Februari 2004",
+            Sosmed: "@Goldwyn_raymond",
+            Quotes: "Ikan Hiu Tabrak Perahu. If I love you, you love me too?",
+            photo: Goldwyn,
+            class: 'IPA'
+        },
+        {
+            Nama: "Melvern Julius",
+            TTL: "Pekanbaru, 28 Mei 2004",
+            Sosmed: "@melvernj28",
+            Quotes: "terlambat kusadari kau teramat berarti. terlambat tuk kembali dan tuk menanti kesempatan kedua,yang tak kan mungkin pernah ada",
+            photo: Melvern,
+            class: 'IPA'
+        },
+        {
+            Nama: "Angky Akdi Frandy Putrakelana",
+            TTL: "Pekanbaru, 1 April 2004",
+            Sosmed: "@angkyakdi",
+            Quotes: "Pernah gak kalian makan gorengan 5 ngakunya gpp padahal lagi sakit sakitnya?",
+            photo: Angky,
+            class: 'IPA'
+        },
+        {
+            Nama: "Felix Anderson",
+            TTL: "Pekanbaru, 15 Oktober 2004",
+            Sosmed: "@felix_anderson1504",
+            Quotes: "if love was a story book we’d meet on the very first page",
+            photo: FA,
+            class: 'IPA'
+        },
+        {
+            Nama: "Richard Kesumah",
+            TTL: "Pekanbaru, 02 Agustus 2004",
+            Sosmed: "@richardkesumah",
+            Quotes: "Klo senggang call dong.... nomorku masih sama kok",
+            photo: Richard,
+            class: 'IPA'
+        },
+        {
+            Nama: "Nicholas Hartono Putra",
+            TTL: "Pekanbaru, 19 Februari 2004",
+            Sosmed: "@nicholashartono7",
+            Quotes: "Saat ini aku sedang berada di titik yang serba salah. Ingin melangkah tapi ragu, hendak mundur tapi tak mau",
+            photo: Nicholas,
+            class: 'IPA'
+        },
+        {
+            Nama: "Michele Lie",
+            TTL: "Pekanbaru, 17 Januari 2005",
+            Sosmed: "@__michelie__",
+            Quotes: "Faber est sue quisque fortunae",
+            photo: Milie,
+            class: 'IPA'
+        },
+        {
+            Nama: "Felix Gelintino",
+            TTL: "Pekanbaru, 30 Januari 2004",
+            Sosmed: "@felixgel29",
+            Quotes: "Semoga mamah sama papah bangga sama aku.",
+            photo: FG,
+            class: 'IPS'
+        },
+        {
+            Nama: "Ribka Gabriella Febriyanti Adams",
+            TTL: "Bogor, 24 Februari 2005",
+            Sosmed: "@gabriellarbcca",
+            Quotes: "Do you know what the Little Mermaid and I have in common? We both want to be part of your world.",
+            photo: aby,
+            class: 'IPA'
+        },
+        {
+            Nama: "Calvin Adrian Hernando",
+            TTL: "Pekanbaru, 10 November 2004",
+            Sosmed: "@calvin_h7",
+            Quotes: "There are no accidents",
+            photo: Calvin,
+            class: 'IPS'
+        },
+        {
+            Nama: "Keefe Melvern Lee",
+            TTL: "Pekanbaru, 4 November 2004",
+            Sosmed: "@keefe__m",
+            Quotes: "yes i know my name is keefe not wahyu , but i keep my love only for you",
+            photo: Keefe,
+            class: 'IPA'
+        },
+        {
+            Nama: "Willyanto",
+            TTL: "Pekanbaru, 07 November 2004",
+            Sosmed: "@willyanto_wu",
+            Quotes: "Take the risk or lose the chance",
+            photo: Willyanto,
+            class: 'IPS'
+        },
+        {
+            Nama: "Steven Wijaya",
+            TTL: "Pekanbaru, 05 Februari 2003",
+            Sosmed: "@stevenwijaya2003",
+            Quotes: "Work hard and stay strong",
+            photo: Steven,
+            class: 'IPS'
+        },
+        {
+            Nama: "Irene Aulia",
+            TTL: "Pekanbaru, 18 Januari 2005",
+            Sosmed: "@irene.aulia",
+            Quotes: "Someday we will say, these were the best years of our life",
+            photo: Irene,
+            class: 'IPS'
+        },
+        {
+            Nama: "Ellden Rieyesco Chow",
+            TTL: "Surabaya, 01 Oktober 2004",
+            Sosmed: "@Ellden_rieyesco_chow",
+            Quotes: "Tong kosong nyaring bunyinya, tapi kalau hati kamu kosong aku mau kok jadi isinya",
+            photo: Elves,
+            class: 'IPS'
+        },
+        {
+            Nama: "Enrique Aurelius",
+            TTL: "Pekanbaru, 08 Oktober 2004",
+            Sosmed: "@enriq.a",
+            Quotes: "See the good",
+            photo: Enriq,
+            class: 'IPS'
+        },
+        {
+            Nama: "Felix Evan Songisty",
+            TTL: "Pekanbaru, 04 November 2004",
+            Sosmed: "@felixevan45",
+            Quotes: "Always remember dying is gay. To avoid dying go to this link https://youtube.com/watch?v=dQw4w9WgXcQ ",
+            photo: FE,
+            class: 'IPS'
+        },
+        {
+            Nama: "Erick Alexander",
+            TTL: "Pekanbaru, 15 Agustus 2004",
+            Sosmed: "@erickalex15",
+            Quotes: "dua tiga tutup botol , goodbye school",
+            photo: Erick,
+            class: 'IPA'
+        },
+        {
+            Nama: "Widya Winata",
+            TTL: "Pekanbaru, 29 Juni 2004",
+            Sosmed: "@Widyawinataa",
+            Quotes: "Ikan hiu makan tomat, haleluya tamat",
+            photo: Widya,
+            class: 'IPA'
+        },
+        {
+            Nama: "David Adryan",
+            TTL: "Pekanbaru, 08 November 2004",
+            Sosmed: "@davidadryan_",
+            Quotes: "being a clown isn't so bad",
+            photo: David,
+            class: 'IPA'
+        },
+        {
+            Nama: "Vallencia Anggelica",
+            TTL: "Pekanbaru, 13 Februari 2004",
+            Sosmed: "@vallggelica",
+            Quotes: "Don't lost to tomorrow.",
+            photo: Vallencia,
+            class: 'IPA'
+        },
+        {
+            Nama: "Felix Giovanni Chen",
+            TTL: "Pekanbaru, 28 Maret 2004",
+            Sosmed: "@FelixGChen",
+            Quotes: "Golden opportunities are often missed by many people because at first glance it looks like an ordinary and trivial thing",
+            photo: FGC,
+            class: 'IPS'
+        },
+        {
+            Nama: "Patrick Lie",
+            TTL: "Pekanbaru, 24 Januari 2005",
+            Sosmed: "@patrickliee_",
+            Quotes: "Satu dua tiga sayang semuanya",
+            photo: Patrick,
+            class: 'IPS'
+        },
+        {
+            Nama: "Enrique Aurelius",
+            TTL: "Pekanbaru, 08 Oktober 2004",
+            Sosmed: "@enriq.a",
+            Quotes: "See the good",
+            photo: Enriq,
+            class: 'IPS'
+        },
+        {
+            Nama: "Vivin Angelina",
+            TTL: "Pekanbaru, 08 Oktober 2004",
+            Sosmed: "@viivviin_",
+            Quotes: "Naik 1kg lebih mudah daripada turunin 0,5kg🙏🏻",
+            photo: Vivin,
+            class: 'IPA'
+        },
+        {
+            Nama: "Jillian",
+            TTL: "Pekanbaru, 27 Februari 2004",
+            Sosmed: "@J1.n17",
+            Quotes: "Enjoy the little things",
+            photo: Jillian,
+            class: 'IPA'
+        },
+        {
+            Nama: "Felix Bryan",
+            TTL: "Pekanbaru, 25 Oktober 2004",
+            Sosmed: "@xmtthwkm",
+            Quotes: "I distance myself from bullshit.",
+            photo: FB,
+            class: 'IPA'
+        },
+        {
+            Nama: "Khatrina Kelly Ongso",
+            TTL: "Pekanbaru, 5 September 2004",
+            Sosmed: "@khatrina_roi",
+            Quotes: "Sometimes you oughta give up on someone, not because you don't care. It's because they don't.",
+            photo: Katty,
+            class: 'IPS'
+        },
+        {
+            Nama: "Mikolas Shivano",
+            TTL: "Pekanbaru, 22 April 2004",
+            Sosmed: "@mshivano",
+            Quotes: "Jangan menyesal yah!",
+            photo: Miko,
+            class: 'IPA'
+        },
+        {
+            Nama: "Vinsson Khosasih",
+            TTL: "Pekanbaru, 11 Agustus 2004",
+            Sosmed: "@vinssonk",
+            Quotes: "Barudak Welllll",
+            photo: Vinson,
+            class: 'IPS'
+        },
+        {
+            Nama: "Winston Bernessa Junio",
+            TTL: "Pekanbaru, 5 Juni 2004",
+            Sosmed: "@winstonbj_",
+            Quotes: "Barangsiapa yang bekerja dengan penuh semangat, keringat, dan air mata di hari Senin, niscaya di keesokan harinya adalah hari Selasa",
+            photo: Winston,
+            class: 'IPA'
+        },
+        {
+            Nama: "Queen Natashya",
+            TTL: "Medan, 15 Juli 2004",
+            Sosmed: "@qqueen_nat",
+            Quotes: "Our prime purpose in this life is to help others. And if you can't help them, at least don't hurt them.",
+            photo: Queen,
+            class: 'IPS'
+        },
+    ];
+    return (<div class="container2">
+        <h1 style={{fontSize: "50px"}}>Year Book 2021/2022</h1>
+        <img src={Witama} />
+        <div class="container">
+            {
+            Student.map((x, index) => {
+                return <div class="peoplePhoto" style={{display:"block"}}>
+                    <img src={x.photo} />
+                    <h2>{x.Nama}</h2>
+                    <h3>TTL : {x.TTL}</h3>
+                    <h3>Sosmed : {x.Sosmed}</h3>
+                    <h3>Quotes : {x.Quotes}</h3>
+                    <h1>XII {x.class}</h1>
+                </div>
+            })
+        }
+        </div>
+        
+        
+    </div>)
+}
